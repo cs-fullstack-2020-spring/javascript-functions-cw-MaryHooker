e5c925e0508d7cc8a763f02ed13e5c4de162c6ba
MaryHooker <mc020791@yahoo.com>
2020-02-21 14:57:14 -0600

    Functions

--- script.js
+++ script.js
@@ -11,13 +11,43 @@
 // printNumbers();
 
 //*****************************************************
-// ### Problem 2
-// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+// // ### Problem 2
+// // Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
 
-function userName(lastName,firstName){
-    return (lastName+firstName);
+// function userName(lastName,firstName){
+//     return (lastName+firstName); 
+// }
+
+// let together=userName("Hooker","Mary");
+// console.log(together)
+
+// //not finished
+//*****************************************************
+// // ### Problem 3
+// // Create a function in your program that counts from 0 to [NUMBER]
+// //Prompt user to enter number and save in variable
+// let userNumber=prompt("Enter a number:");
+// //make function to with a for loop
+// function count(){
+//     for(i=0;i<userNumber;i++){
+//         console.log(i)
+//     }
+// }
+// //call function to run
+// count();
+//******************************************************
+// ### Problem 4
+// Create a function called checkPassword. Send two string variables to the checkPassword function to check if the strings are equal. Return true if they are equal and false if they are not equal. Print the function's return value.
+
+function checkPassword(name1,name2){
+    if(name1==name2){
+        console.log("true");
+    } else if(name1!=name2){
+        console.log("false");
+    } else{
+        return(name1,name2);
+    }
 }
 
-console.log(userName("Hooker","Mary"))
-
-
+checkPassword("bob","tom");
+checkPassword("bob","bob");
