8058def1e57208afd66845084801e85f9e085fa1
MaryHooker <mc020791@yahoo.com>
2020-02-21 14:21:34 -0600

    1 and some of 2

---
+++ script.js
@@ -0,0 +1,23 @@
+// // ### Problem 1
+// // Create a ```printNumbers``` function to print integers from -25 to 20.
+
+// function printNumbers(){
+//     //Create a for loop that starts i less than zero/create a conditional that says to run while i is less than or equal to 20/and continue to add one to your iterator
+//     for(let i=-25;i<=20;i++){
+//         console.log(i);
+//     }
+// }
+// //call your function to run
+// printNumbers();
+
+//*****************************************************
+// ### Problem 2
+// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+
+function userName(lastName,firstName){
+    return (lastName+firstName);
+}
+
+console.log(userName("Hooker","Mary"))
+
+
